3c4cf8bdf6e73d3597cdbc975e31846494966a20
LNUAKX <lukmaannadaf@gmail.com>
2023-04-27 15:56:17 +0530

    started

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyparser=require("body-parser");
 const http=require('https');
 const { log } = require('console');
-
+require("dotenv").config();
 const app=express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -12,15 +12,15 @@ app.set('view engine','ejs');
 app.get("/",(req,res)=>{
     res.render("index");
 })
-
+const appid=process.env.APPID;
 app.post('/',(req,res)=>{
     const cityName=req.body.cityname;
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=3fc9ff4b06dea7566fa3f24cf5006a07&units=metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+appid+"&units=metric";
     http.get(url,(response)=>{
         response.on("data",(data)=>{
             const weatherData=JSON.parse(data);
             const city=weatherData.name;
-            const windspeed=weatherData.wind.speed;
+            // const windspeed=weatherData.wind.speed;
             const discription=weatherData.weather[0].description;
             const humidity=weatherData.main.humidity;
             const iconcode=weatherData.weather[0].icon;
@@ -31,7 +31,7 @@ app.post('/',(req,res)=>{
             const today=new Date();
             const currenttime = today.getHours() + ":" + today.getMinutes() ;
 
-            res.render("weather",{cityn:city,temp:tempreature,disc:discription,wind:windspeed,humi:humidity,imgsrc:imgUrl,time:currenttime});
+            res.render("weather",{cityn:city,temp:tempreature,disc:discription,wind:45,humi:humidity,imgsrc:imgUrl,time:currenttime});
             
 
         })
